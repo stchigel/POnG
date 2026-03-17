77ba58c57d8b44ea988339009a69e03c878655e2
stchigel <alumno26.stchigel.grimaldi.nicolas@ipm.edu.ar>
2026-03-17 18:41:33 -0300

    Funcionalidad del juego casi hecha, detalles para hacer: Que el juego termine al llegar a cierta cantidad de goles, que el bot solo se mueva cuando la pelota va hacia su lado y agregar el modo de agua

--- src/Game/game.gd
+++ src/Game/game.gd
@@ -1,7 +1,22 @@
 extends Node2D
 
+var EnemyScene = preload("res://src/OpponentPaddle/opponent_paddle.tscn")
+var Player2Scene = preload("res://src/Player2Paddle/player_2_paddle.tscn")
+
 var PScore = 0
 var EScore = 0
+var Mode = true
+# true es VS IA, false es 1v1 local
+
+func _ready():
+	if Mode:
+		var enemy = EnemyScene.instantiate()
+		enemy.position = Vector2(1056, 320)
+		add_child(enemy)
+	else:
+		var player2 = Player2Scene.instantiate()
+		player2.position = Vector2(1056, 320)
+		add_child(player2)
 
 func _on_player_goal_body_entered(body: Node2D) -> void:
 	EScore += 1
@@ -16,16 +31,16 @@ func _on_enemy_goal_body_entered(body: Node2D) -> void:
 func _process(delta):
 	$PScore.text = str(PScore)
 	$EScore.text = str(EScore)
-	$TimerLabel.text = str(round($Timer.time_left * 10.0) / 10.0)
 
 func _on_timer_timeout() -> void:
 	get_tree().call_group("BallGroup", "restart_ball")
-	$TimerLabel.visible = false
 	
 func scored():
 	$Ball.position = Vector2(576, 320)
 	get_tree().call_group("BallGroup", "stop_ball")
 	$Timer.start()
-	$TimerLabel.visible = true
 	$PlayerPaddle.position.x = 96
-	$OpponentPaddle.position.x = 1056
+	if Mode:
+		$OpponentPaddle.position.x = 1056
+	else: 
+		$Player2Paddle.position.x = 1056
---
+++ src/Player2Paddle/player_2_paddle.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var speed = 500
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down") :
+		velocity.y += 1
+	move_and_collide(velocity * speed * delta)
